a8c24bc57adc71315ba25cbf3eb2fc2033328bb9
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-03-22 13:18:54 +0100

    Cómo procesa Laravel una petición

---
+++ resources/views/entradas/index.blade.php
@@ -0,0 +1,25 @@
+{{-- resources/views/entradas/index.blade.php --}}
+
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Entradas</h1>
+
+    <table border="1">
+        <tr>
+            <th>Título</th>
+            <th>Texto</th>
+            <th>Fecha</th>
+        </tr>
+        @foreach($entradas as $entrada)
+            <tr>
+                <td>{{ $entrada->titulo }}</td>
+                <td>{{ $entrada->texto }}</td>
+                <td>{{ $entrada->fecha }}</td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mi blog</title>
+</head>
+<body>
+
+@yield('content')
+
+<footer>@yield('footer')</footer>
+
+</body>
+</html>
